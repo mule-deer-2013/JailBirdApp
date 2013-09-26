de7154a2cd9fb9e8b917b919ad6e75a49a3bb6d0
Absoludacris06 <nicholas.saldivar@gmail.com>
2013-09-26 14:28:52 -0700

    add user model & migration

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+
+	has_secure_password
+
+	validates :email, uniqueness: true
+	
+
+end
---
+++ db/migrate/20130926211952_create_users.rb
@@ -0,0 +1,7 @@
+class CreateUsers < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
