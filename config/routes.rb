a59940a7fc0b4b4911b2a14628a181b0569fc1a6
Elaine Tsai <et.elainetsai@gmail.com>
2013-09-27 15:34:05 -0700

    add texting voicemail and fix merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Jailbird::Application.routes.draw do
   match '/api/calls', to: 'api#calls'
   match '/api/group_sms', to: 'api#group_sms'
   match '/api/phonebook', to: 'api#phonebook'
+  match '/api/transcribe_call', to: 'api#transcribe_call'
 
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
