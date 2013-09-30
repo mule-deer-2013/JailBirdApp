a1de10adaa6784cc86768f6fdab783c9d2066b27
audibleblink <virtualsnyper@gmail.com>
2013-09-30 13:19:34 -0700

    combined css pages for registration panel animation. also changed registration.js handler to listen for buttons instead of anchors

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -1,6 +1,6 @@
 var Panel = {
   init: function() {
-    $('.panel').on('click', 'a', this.togglePanelForm);
+    $('.panel').on('click', '.flipper', this.togglePanelForm);
   },
 
   togglePanelForm: function(e) {
