0cd13bf77091f73d6e72bd99c7ea0bbab2a9430a
Charlie Young <charlie_young@Charlies-MacBook-Air.local>
2013-10-01 23:43:12 -0700

    cleaned up pagination js

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,32 +1,20 @@
 $(document).ready(function(){
 
-$('#groups_paginate').on('click', 'img', function(e){
-	e.preventDefault();
-	var link = $(this).parent().attr("href")
-	var match = /\d+$/
-	var pageNumber = match.exec(link)
+	$('#groups_paginate').on('click', 'img', function(e){
+		e.preventDefault();
+		$('#groups_paginate').animate({ "left": "-=50px" }, "slow" );
+		var link = $(this).parent().attr("href")
+		var match = /\d+$/
+		var pageNumber = match.exec(link)
 
-	console.log('I was clicked!')
-	$.ajax({
-		url: 'paginate/groups?page='+pageNumber,
-    type: 'get',
+		console.log('I was clicked!')
+		$.ajax({
+			url: 'paginate/groups?page='+pageNumber,
+	    type: 'get',
+		})
+		.done(function(response) {
+			console.log(response)
+			$('#groups_paginate').html(response);
+		})
 	})
-	.done(function(response) {
-		console.log(response)
-		$('#groups_paginate').html(response);
-	})
-})
-
-// function editContact(e){
-//   e.preventDefault()
-//   $.ajax({
-//     url: 'contacts/'+$(this).attr('id')+'/edit',
-//     type: 'get',
-//   })
-//   .done(function(response) {
-//     $('#modalBox').append(response);
-//   })
-//   $('#modalBox').trigger('openModal')
-// }
-
 });
